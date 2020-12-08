52e4b6bcdab73b9151d97a592d84e70de26464f1
beatrizodorcik <beaodorcik@gmail.com>
2020-12-08 18:03:06 -0300

    Meu primeiro CRUD <3

---
+++ blogBea/src/main/java/org/generation/blogBea/BlogBeaApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogBea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBeaApplication.class, args);
+	}
+
+}
---
+++ blogBea/src/test/java/org/generation/blogBea/BlogBeaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogBea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
