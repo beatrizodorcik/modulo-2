836db0caf9dedab2f0ba174d53643f47b01822ea
beatrizodorcik <beaodorcik@gmail.com>
2020-12-09 20:15:06 -0300

    Spring Boot - E-commerce - Farmárcia10

---
+++ ecommercefarmacia/src/main/java/com/farmacia/ecommercefarmacia/EcommercefarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.ecommercefarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercefarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercefarmaciaApplication.class, args);
+	}
+
+}
---
+++ ecommercefarmacia/src/test/java/com/farmacia/ecommercefarmacia/EcommercefarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.ecommercefarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercefarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
